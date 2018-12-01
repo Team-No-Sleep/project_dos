54093440371f031ee00fa16e9bc59e26f4d6ba8e
kpcruz <kelvinmembership@gmail.com>
2018-11-30 22:45:08 -0800

    cleaned up code

--- public/js/chatbot.js
+++ public/js/chatbot.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const dialogflow = require('dialogflow');
 
-
 const sessionId = '123'; //pull id from mysql? Needed for a continuous conversation
 const languageCode = 'en-US';
 let userQuery = 'I need a job'
@@ -36,9 +35,10 @@ class DialogFlow {
 		}
 		try {
 			let responses = await this.sessionClient.detectIntent(request)			
-			console.log('DialogFlow.sendTextMessageToDialogFlow: Detected intent');
 			fulfillmentText = responses[0].queryResult.fulfillmentText;
 			queryText = responses[0].queryResult.queryText;
+            
+            console.log('DialogFlow.sendTextMessageToDialogFlow: Detected intent');
 			console.log(`  Query: ${queryText}`);
 			console.log(`  Response: ${fulfillmentText}`);
 			console.log(responses)
@@ -50,51 +50,19 @@ class DialogFlow {
 		}
 	}
 }
+
+// EXAMPLE USE
+// ===========
+
+// Initiate a new clientSession
 agent = new DialogFlow()
+//userQuery is what the user types in, sessionId is for a continuous conversation
+//it will return the var fulfillmentText and var queryText for use in the chatbot ui
 agent.sendTextMessageToDialogFlow(userQuery, sessionId)
 
 
+module.exports = dialogflow;
 
-// const sessionId = '123'; //pull id from mysql
-// const languageCode = 'en-US';
-// const projectId = process.env.DIALOGFLOW_PROJECT_ID;
-// let query = 'I need a job'
 
-// let config = {
-//     credentials: {
-//         private_key: process.env.DIALOGFLOW_PRIVATE_KEY,
-//         client_email: process.env.DIALOGFLOW_CLIENT_EMAIL
-//     }
-// }
 
-// const sessionClient = new dialogflow.SessionsClient(config);
-// const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-// // The text query request.
-// const request = {
-//     session: sessionPath,
-//     queryInput: {
-//         text: {
-//             text: query,
-//             languageCode: languageCode,
-//         },
-//     },
-// };
-
-// // Send request and log result
-// sessionClient
-//     .detectIntent(request)
-//     .then(responses => {
-//         console.log('Detected intent');
-//         const result = responses[0].queryResult;
-//         console.log(` -Query: ${result.queryText}`);
-//         console.log(` -Response: ${result.fulfillmentText}`);
-//         if (result.intent) {
-//             console.log(` -Intent: ${result.intent.displayName}`);
-//         } else {
-//             console.log(` -No intent matched.`);
-//         }
-//     })
-//     .catch(err => {
-//         console.error('ERROR:', err);
-//     });
