4d5244d87e30e1a3dd3587643ed7fff3f574f2d1
Lakshmi Strom <lakshmialeman@gmail.com>
2018-12-07 03:36:02 -0800

    succesfully connected dialogFlow to socket.io to display responses using botui

---
+++ routes/chatbot.js
@@ -0,0 +1,95 @@
+require("dotenv").config();
+const dialogflow = require("dialogflow");
+const uuid = require("uuid");
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function textQuery(input, sessionId) {
+  // A unique identifier for the given session
+  sessionId = sessionId || uuid.v4();
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient();
+  const sessionPath = sessionClient.sessionPath(
+    process.env.DIALOGFLOW_PROJECT_ID,
+    sessionId
+  );
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: input,
+        // The language used by the client (en-US)
+        languageCode: "en-US"
+      }
+    }
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(result);
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log("  No intent matched.");
+  }
+  return {
+    response: result.fulfillmentText,
+    sessionId: sessionId
+  };
+}
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function eventQuery(input, sessionId) {
+  // A unique identifier for the given session
+  sessionId = sessionId || uuid.v4();
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient();
+  const sessionPath = sessionClient.sessionPath(
+    process.env.DIALOGFLOW_PROJECT_ID,
+    sessionId
+  );
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      event: {
+        // The query to send to the dialogflow agent
+        name: input,
+        // The language used by the client (en-US)
+        languageCode: "en-US"
+      }
+    }
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(result);
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log("  No intent matched.");
+  }
+  return {
+    response: result.fulfillmentText,
+    sessionId: sessionId
+  };
+}
+module.exports = { textQuery, eventQuery };
